be4ac3aed86cf29c754cd8fc7991a490cbdc58b9
yahav <f54006@gantavne.ort.org.il>
2025-09-11 16:31:59 +0300

    main menu finished

--- final-project/final-project/Pages/LoginPage.xaml.cs
+++ final-project/final-project/Pages/LoginPage.xaml.cs
@@ -20,31 +20,11 @@ namespace final_project.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainMenuPage : Page
+    public sealed partial class LoginPage : Page
     {
-        public MainMenuPage()
+        public LoginPage()
         {
             this.InitializeComponent();
-        }
-
-        private void StartButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(ModePage));
-        }
-
-        private void SettingsButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(LoginPage));
-        }
-
-        private void SignupButton_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(SignupPage));
         }
     }
 }
