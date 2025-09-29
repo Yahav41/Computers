34a3467865eabcc16892655c0a2fc8248e2817be
yahav <f54006@gantavne.ort.org.il>
2025-09-29 17:00:41 +0300

    GameEngine started

---
+++ final-project/GameEngine/Objects/GameMovingObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace GameEngine.Objects
+{
+    public abstract class GameMovingObject : GameObject
+    {
+        protected double _speedX;
+        protected double _speedY;
+        protected double _accelerationX;
+        protected double _accelerationY;
+        protected GameMovingObject(Canvas scene, string fileName, double x, double y, double size) : base(scene, fileName, x, y, size)
+        {
+            Stop();
+        }
+        public override void Render()
+        {
+            _x += _speedX;
+            _y += _speedY;
+            _speedX += _accelerationX;
+            _speedY += _accelerationY;
+            base.Render();
+        }
+
+        public void Stop()
+        {
+            _speedX = _speedY = _accelerationX = _accelerationY = 0;
+        }
+    }
+}
---
+++ final-project/GameEngine/Objects/GameObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace GameEngine.Objects
+{
+    public abstract class GameObject
+    {
+        protected double _x;
+        protected double _y;
+        protected Image _image;
+        protected string _fileName;
+        protected Canvas _scene;
+        public bool Collisional { get; set; } = true;
+        public bool IsObjectCreated { get; set; } = false;
+
+        protected GameObject(Canvas scene,string fileName, double x, double y, double size)
+        {
+            _scene = scene;
+            _fileName = fileName;
+            _x = x;
+            _y = y;
+            _image = new Image();
+            _image.Width = size;
+            Render();
+            SetName(_fileName);
+            _scene.Children.Add(_image);
+        }
+
+        protected void SetName(string fileName)
+        {
+            _image.Source = new BitmapImage(new Uri($"ms-appx:///Assets/{fileName}"));
+        }
+
+        public virtual void Collide(GameObject gameObject)
+        {
+
+        }
+
+        public virtual void Render() 
+        {
+            Canvas.SetLeft(_image,_x);
+            Canvas.SetTop(_image,_y);
+            Rect();
+        }
+
+        public virtual Rect Rect()
+        {
+            return new Rect(_x,_y, _image.ActualWidth, _image.ActualHeight);
+        }
+
+    }
+}
