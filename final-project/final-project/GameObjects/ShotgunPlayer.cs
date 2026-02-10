2de50222e5770f1d17a8b7fd380f0c5b9656e698
yahav <f54006@gantavne.ort.org.il>
2026-02-10 10:19:43 +0200

    not crashing

--- final-project/final-project/GameObjects/ShotgunPlayer.cs
+++ final-project/final-project/GameObjects/ShotgunPlayer.cs
@@ -22,7 +22,7 @@ namespace final_project.GameObjects
             _scene.AddObject(new Bullets(Image.Rotation, muzzleX, muzzleY, 10, _scene, 50));
 
             int playerId = _isLeft ? GameConstants.leftPlayer : GameConstants.rightPlayer;
-            Manager.Events.OnBulletFired(muzzleX, muzzleY, (float)angle, 50, playerId);
+            Manager.Events.OnBulletFired?.Invoke(muzzleX, muzzleY, (float)angle, 50, playerId);
         }
 
         public override int Type()
